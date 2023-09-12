52292419b35fcb39632c46a528fce1b339c24554
Shivani1810 <83349472+Shivani1810@users.noreply.github.com>
2023-09-12 07:44:18 +0000

    Datatypes

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict"; //treat all JS code as newer version
+
+//alert(3 + 3)    //We are using nodeJs, not browser i.e, it will not give output
+
+console.log(3 +
+    3);         //code reusability and readibility should be high
+
+console.log("Shivani"); 
+
+let name = "Shivani"
+let age = 18
+let isLoggedIn = false
+
+// Number => 2^53
+// BigInt
+// string => ""
+// boolean => true/false
+// null => Standalone value
+// Undefined => Value is not assigned yet
+// symbol => unique
+
+
+//object
+
+console.log(typeof age);
+console.log(typeof null);           //object
+console.log(typeof undefined);
+

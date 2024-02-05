69906e2cd014b017c21f45993b0a5d451805a701
Shivani1810 <83349472+Shivani1810@users.noreply.github.com>
2024-02-05 14:49:55 +0000

    Mouse cursor Hover

---
+++ PortfolioWeb/MultipleCursor/script.js
@@ -0,0 +1,29 @@
+var elem1 = document.querySelectorAll(".elem");
+
+elem1.forEach(function(val){
+    val.addEventListener("mouseenter", function(){
+        val.childNodes[3].style.opacity = 1;
+    });
+
+    val.addEventListener("mouseleave", function(){
+        val.childNodes[3].style.opacity = 0;
+    });
+
+    val.addEventListener("mousemove", function(dets){
+        val.childNodes[3].style.left = dets.x+"px"
+    });
+});
+// var elemImg = document.querySelector("#elem1 img");
+
+// elem1.addEventListener("mousemove", function(dets){
+//         elemImg.style.left = dets.x + "px"   
+//         elemImg.style.top = dets.y + "px"
+// });
+
+// elem1.addEventListener("mouseenter", function(dets){
+//     elemImg.style.opacity = 1;
+// });
+
+// elem1.addEventListener("mouseleave", function(dets){
+//     elemImg.style.opacity = 0;
+// });
